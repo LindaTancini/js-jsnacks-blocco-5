80c8dc572656f0c4c7ebc1e3a580bb3cc625e45a
LindaTancini <lindatancini95@gmail.com>
2025-03-12 17:14:55 +0100

    feat: correct console.log with filter

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,7 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
-console.log("---FOR EACH---");
+console.log("---FILTER---");
 const numPari = nums.filter((element) => element % 2 === 0);
 console.log(numPari);
 
