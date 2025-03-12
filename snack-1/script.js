59601f39f15cde1a98639255843d9787ab5ad05c
LindaTancini <lindatancini95@gmail.com>
2025-03-12 15:00:41 +0100

    feat: added bonus snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,7 +2,13 @@ const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
-
+console.log("---FOR EACH---");
 names.forEach((element) => {
   console.log(element);
 });
+
+//CON CILCLO FOR
+console.log("---CICLO FOR---");
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
