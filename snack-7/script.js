8fd794804c5c662fcb2c07781828c086ff95dfdf
LindaTancini <lindatancini95@gmail.com>
2025-03-12 17:19:02 +0100

    feat: added bonus snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,5 +21,14 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+console.log("---FIND---");
 const idTwo = students.find((element) => element.id === 2);
 console.log(idTwo);
+
+// CON CICLO FOR
+console.log("---CICLO FOR---");
+for (let i = 0; i < students.length; i++) {
+  if (students[i].id === 2) {
+    console.log("I dati dello studente con id 2 sono:", students[i]);
+  }
+}
