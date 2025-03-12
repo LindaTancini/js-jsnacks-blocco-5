48e78f49809192ead48fe3d8c2b4c1fbc2c41c50
LindaTancini <lindatancini95@gmail.com>
2025-03-12 15:08:15 +0100

    feat: added bonus snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,5 +2,14 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+console.log("---FOR EACH---");
 const numbersAddOne = numbers.map((element) => element + 1);
 console.log(numbersAddOne);
+
+//CON CICLO FOR
+console.log("---CICLO FOR---");
+let newNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+  newNumbers.push(numbers[i] + 1);
+}
+console.log(newNumbers);
